b7b54b9b4b82c8a8c2353a68647a8488dec34e03
lilian waweru <lilowesh.lw@gmail.com>
2019-04-06 18:01:16 +0300

    added a multiple saver

--- login.py
+++ login.py
@@ -10,3 +10,9 @@ class Login:
         self.password = password
         self.new_password = new_password
         self.confirm_password = confirm_password
+
+     def save_login(self):
+            '''
+        save_login method saves contact objects into login
+        '''
+        Login.login.append(self)
--- login_test.py
+++ login_test.py
@@ -1,4 +1,4 @@
-import run_unittest
+import unittest
 from login import Login
 
 class TestLogin(unittest.TestCase):
@@ -13,4 +13,31 @@ class TestLogin(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_login = Login("lilomuso","lilo89","lilo89","lilo89") # create login object
-        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_login.username,"lilomuso")
+        self.assertEqual(self.new_login.password,"lilo89")
+        self.assertEqual(self.new_login.new_password,"lilo89")
+        self.assertEqual(self.new_login.confirm_password,"lilo89")
+
+    def test_save_login(self):
+        '''
+        test_save_login test case to test is the login object is saved into the login
+        '''
+        self.new_login.save_login()#saving the new user
+        self.assertEqual(len(Login.login),1)
+
+    def test_save_multiple_login(self):
+        '''
+        test_save_multiple_login to check if we can save multiple login object to our user
+        '''
+        self.new_login.save_login()
+        test_login = Login("Test","login","lilo89","lilo89")#new login
+        test_login.save_login()
+        self.assertEqual(len(Login.login),2)   
+
+
+
